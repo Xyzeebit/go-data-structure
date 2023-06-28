da31e94ade704ec438ecf59d29e11ff94a669434
xyzeebit <gotodonald@gmail.com>
2023-06-28 06:39:00 +0100

    Changing struct internal data through method pointers

--- basics/structs/struct_methods.go
+++ basics/structs/struct_methods.go
@@ -15,6 +15,16 @@ func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.width + r.height)
 }
 
+type MyInt int
+
+func (i MyInt) increment1() {
+	i = i + 1
+}
+
+func (i *MyInt) increment2() {
+	*i = *i + 1
+}
+
 func main() {
 	r := Rectangle{width: 15, height: 10}
 	fmt.Println("Area:", r.Area())
@@ -22,4 +32,12 @@ func main() {
 
 	ptr := &Rectangle{width: 8.87, height: 12.25}
 	fmt.Printf("Area: %.2f\nPerimeter :%.2f\n", ptr.Area(), ptr.Perimeter())
+
+	fmt.Println("Changing type internal data")
+	var n MyInt = 1
+	fmt.Println("Before increment()", n)
+	n.increment1()
+	fmt.Println("After increment1()", n)
+	n.increment2()
+	fmt.Println("After increment2()", n)
 }
