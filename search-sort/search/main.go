faf19efe0545c99f0c495a7d261fe10cc15cce5a
xyzeebit <gotodonald@gmail.com>
2023-07-02 16:04:00 +0100

    Implement linear search for sorted arrays

--- search-sort/search/main.go
+++ search-sort/search/main.go
@@ -6,12 +6,28 @@ func main() {
 	arr := []int{2, 8, 1, 9, 10, 1, 5, 7}
 	v := 5
 	find := linearSearch(arr, v)
-	fmt.Printf("search in %v, %d : ", arr, v)
-	fmt.Println(find)
+	fmt.Printf("search in %v for %d : %d\n", arr, v, find)
+
+	arr2 := []int{1, 2, 4, 6, 7}
+	v = 2
+	find = linearSearchSorted(arr2, v)
+	fmt.Printf("search in %v for %d : %d\n", arr2, v, find)
 }
 
 func linearSearch(list []int, v int) int {
 	for i, a := range list {
+		if v == a {
+			return i
+		}
+	}
+	return -1
+}
+
+func linearSearchSorted(list []int, v int) int {
+	for i, a := range list {
+		if v < a {
+			return -1
+		}
 		if v == a {
 			return i
 		}
